7a35c1a34202f827622b89e98beeb5a8399009c5
vika <@>
2021-09-08 14:40:52 +0300

    Add: load data and show additional onformation

---
+++ src/components/userInfo/userInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UserInfo = ({user}) => {
+    return (<div>
+            {user && <div>
+                <div>Profile info:</div>
+                <div> Selected profile: {`${user.firstName} ${user.lastName}`}</div>
+                <div> Description: {user.description}</div>
+                <div> Address: {user.adress.streetAddress}</div>
+                <div> City: {user.adress.city}</div>
+                <div> State: {user.adress.state}</div>
+                <div> Index: {user.adress.zip}</div>
+
+            </div>}
+        </div>
+    );
+};
+
+export default UserInfo;
